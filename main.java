fdf162ed3e8406341a7e554e7b8edf3783d178cb
Stewl3 <stevecraft2014@outlook.com>
2023-07-10 19:04:53 -0400

    Started input for calculator

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.math.*;
+import java.util.*;
+
+class Main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        String operand = scanner.nextLine();
+        int secondNumber = scanner.nextInt();
+
+    }
+
+}
